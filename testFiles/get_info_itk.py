766306d74ff74296e90e05f31301dcd4eb92b61c
Jacob Edwin Crosby <jacob.edwin.crosby@cern.ch>
2025-04-23 15:37:23 -0500

    removed testFiles

--- testFiles/get_info_itk.py
+++
@@ -1,6 +0,0 @@
-import itkdb
-
-client = itkdb.Client()
-client.user.authenticate()  
-user = client.get("getUser", json={"userIdentity": client.user.identity})
-print([institution["code"] for institution in user["institutions"]])
